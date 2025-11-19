a55df35e64e24e99ffa1bc9ae9327020b5bd3c10
asadbek <asadbek,yoldashev623@gmail.com>
2025-11-19 16:23:13 +0500

    soatt

--- soat/main.js
+++ soat/main.js
@@ -10,21 +10,24 @@ let h = document.querySelector(".h")
 
 // console.log(data);
 
-function clock(){
+function clock() {
     let data = new Date()
     let sec = data.getSeconds()
     let min = data.getMinutes()
     let hour = data.getHours()
 
     // console.log(sec, min, hour);
-    
+
 
     s.style.transform = `rotate(${sec * 6}deg)`
+    m.style.transform = `rotate(${min * 6}deg)`
+    h.style.transform = `rotate(${hour * 12}deg)`
+    setTimeout(() => {
+        clock()
+    }, 1000);
 
- setTimeout(() => {
-    clock()
- }, 1000);
 }
+
 clock()
 
 
