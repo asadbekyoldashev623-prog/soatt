b6e95b14f6dd9e0cb8ce7e564aed3c534f188578
asadbek <asadbek,yoldashev623@gmail.com>
2025-11-26 16:01:35 +0500

    soat

--- soat/main.js
+++ soat/main.js
@@ -50,3 +50,21 @@ for (let i = 0; i < tabsItem.length; i++) {
     })
 }
 
+
+let stopwatchBtn = document.querySelector('.stopwatch__btn')
+function sec() {
+    stopwatchBtn.addEventListener('click', () => {
+       
+if (stopwatchBtn.innerText == 'START') {
+    stopwatchBtn.innerText = 'STOP'
+}
+else if (stopwatchBtn.innerText == 'STOP'){
+    stopwatchBtn.innerText = 'CLEAR'
+}
+else{
+    stopwatchBtn.innerText = "START"
+}
+        
+    })
+}
+sec()
